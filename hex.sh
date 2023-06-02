fa6326e5143fec643e376b5ac41d3418bb52538e
Ayon Bhowmick <ayb224@lehigh.edu>
2023-06-01 23:13:46 -0400

    hex and readme

--- hex.sh
+++ hex.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 m_flag=""
-g_flag=false
+g_flag="-nocuda"
+receptor = ""
 
 # check if pdbFiles folder exists and is not empty
 if [ ! -d "pdbFiles" ]; then
@@ -13,28 +14,39 @@ if [ -z "$(ls -A pdbFiles)" ]; then
     exit 1
 fi
 
+if [ ! -d "resultLogs" ]; then
+    mkdir resultLogs
+fi
+
 # check is hex is installed
 if ! command -v hex &> /dev/null; then
     echo "hex is not installed"
     exit 1
 fi
 
-# check if -m flag is given
+# check if -m and -r flags is given
 if [[ $* != *-m* ]]; then
     echo "-m flag is not given"
     exit 1
 fi
+if [[ $* != *-r* ]]; then
+    echo "-r flag is not given"
+    exit 1
+fi
 
-while getopts 'm:g' OPTIONS; do
+while getopts 'm:gr:l:' OPTIONS; do
     case "$OPTIONS" in
         m) m_flag="$OPTARG"
             ;;
-        g) g_flag=true
+        g) g_flag="-cuda"
             ;;
-        ?) echo "script usage: $(basename \$0) [-m <docking method mac file>] [-g]"
+        r) receptor="$OPTARG"
+            ;;
+        ?) echo "script usage: $(basename \$0) [-m <docking method mac file>] [-r <receptor file>] {-g}"
             exit 1
             ;;
     esac
 done
 
 for file in pdbFiles/*.pdb; do
+    hex $g_flag $receptor $file -e $m_flag -l resultLogs/$file.log
