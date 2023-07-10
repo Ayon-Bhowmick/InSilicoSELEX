9e4f0a8eef150bfe2451785afaee4407c9db0998
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2023-07-09 23:48:36 -0400

    path

--- RNAComposer/RenameFiles.py
+++ RNAComposer/RenameFiles.py
@@ -1,21 +1,24 @@
 import os
 import pickle
 
-# rename files
-with open("name_directory.pkl", "rb") as f:
-    name_directory = pickle.load(f)
-for file in os.listdir(f"{PATH_TO_HERE}\\pdbFiles"):
-    if file.endswith(".pdb"):
-        if (id := file.split(".")[0]).isdigit():
-            file_name = name_directory.get(int(id))
-        else:
-            continue
-        if file_name == None:
-            print(f"Could not find name for {file}")
-        else:
-            try:
-                os.rename(f"{PATH_TO_HERE}\\pdbFiles\\{file}", f"{PATH_TO_HERE}\\pdbFiles\\{file_name}.pdb")
-            except FileExistsError:
-                print(f"File {file_name}.pdb already exists")
-            except Exception as e:
-                print(f"Error renaming {file}: {e}")
+PATH_TO_HERE = "\\".join(os.path.dirname(os.path.abspath(__file__)).split("\\")[:-1])
+
+if __name__ == "__main__":
+    # rename files
+    with open("name_directory.pkl", "rb") as f:
+        name_directory = pickle.load(f)
+    for file in os.listdir(f"{PATH_TO_HERE}\\pdbFiles"):
+        if file.endswith(".pdb"):
+            if (id := file.split(".")[0]).isdigit():
+                file_name = name_directory.get(int(id))
+            else:
+                continue
+            if file_name == None:
+                print(f"Could not find name for {file}")
+            else:
+                try:
+                    os.rename(f"{PATH_TO_HERE}\\pdbFiles\\{file}", f"{PATH_TO_HERE}\\pdbFiles\\{file_name}.pdb")
+                except FileExistsError:
+                    print(f"File {file_name}.pdb already exists")
+                except Exception as e:
+                    print(f"Error renaming {file}: {e}")
