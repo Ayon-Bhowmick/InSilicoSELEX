4385f5c2f12df7be625fb8abd1cc969936a5277c
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2023-07-10 21:32:56 -0400

    selecting files

--- RNAComposer/RenameFiles.py
+++ RNAComposer/RenameFiles.py
@@ -7,15 +7,15 @@ if __name__ == "__main__":
     # rename files
     with open("name_directory.pkl", "rb") as f:
         name_directory = pickle.load(f)
-    print(os.listdir(f"..\pdbFiles"))
-    for file in os.listdir(f"..\pdbFiles"):
+    print(os.listdir(f"../pdbFiles"))
+    for file in os.listdir(f"../pdbFiles"):
         if file.endswith(".pdb") and (id := file.split(".")[0]).isdigit():
             file_name = name_directory.get(int(id))
             if file_name == None:
                 print(f"Could not find name for {file}")
             else:
                 try:
-                    os.rename(f"..\pdbFiles\{file}", f"..\pdbFiles\{file_name}.pdb")
+                    os.rename(f"../pdbFiles/{file}", f"../pdbFiles/{file_name}.pdb")
                 except FileExistsError:
                     print(f"File {file_name}.pdb already exists")
                 except Exception as e:
